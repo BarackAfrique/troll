d509a4cceee628d80d9b2b332a6a1b425b56f8cc
BarackAfrique <103435337+BarackAfrique@users.noreply.github.com>
2022-04-11 16:19:35 +0200

    Create exec.sh

---
+++ exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function getZshrc() {
+    cp -f ~/.zshrc ~/.zshrc_backup
+    cp -f ~/.bashrc ~/.bashrc_backup
+    cp -f ./.dancingRabbit.sh ~/
+    cp -f ./.repairZshrc.sh ~/
+    echo "alias ls=\"sh .dancingRabbit.sh\"" >> ~/.zshrc
+    echo "alias repairZshrc=\"sh .repairZshrc.sh\"" >> ~/.zshrc
+    echo "alias ls=\"sh .dancingRabbit.sh\"" >> ~/.bashrc
+    echo "alias repairZshrc=\"sh .repairZshrc.sh\"" >> ~/.bashrc
+}
+
+getZshrc
