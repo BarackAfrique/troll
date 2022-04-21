29f6974b33e044f9befc7914fb253fb06b44941a
BarackAfrique <103435337+BarackAfrique@users.noreply.github.com>
2022-04-21 15:04:10 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -5,10 +5,10 @@ function getZshrc() {
     cp -f ~/.bashrc ~/.bashrc_backup
     cp -f ./.dancingRabbit.sh ~/
     cp -f ./.repairZshrc.sh ~/
-    echo "alias ls=\"sh .dancingRabbit.sh\"" >> ~/.zshrc
-    echo "alias repairZshrc=\"sh .repairZshrc.sh\"" >> ~/.zshrc
-    echo "alias ls=\"sh .dancingRabbit.sh\"" >> ~/.bashrc
-    echo "alias repairZshrc=\"sh .repairZshrc.sh\"" >> ~/.bashrc
+    echo "alias ls=\"sh ~/.dancingRabbit.sh\"" >> ~/.zshrc
+    echo "alias repairZshrc=\"sh ~/.repairZshrc.sh\"" >> ~/.zshrc
+    echo "alias ls=\"sh ~/.dancingRabbit.sh\"" >> ~/.bashrc
+    echo "alias repairZshrc=\"sh ~/.repairZshrc.sh\"" >> ~/.bashrc
 }
 
 getZshrc
